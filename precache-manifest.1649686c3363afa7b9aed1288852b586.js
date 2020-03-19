c19ec76083d9a3e4f6ed3dff868ccb281b126732
Serhi Skachkov <sergeyskachkov01@gmail.com>
2020-03-19 15:18:33 +0200

    Updates

---
+++ precache-manifest.1649686c3363afa7b9aed1288852b586.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4391859b625e655157366616b42d1347",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "5bbecd2d262d70bd950d",
+    "url": "/gitsearch/static/css/main.528a989a.chunk.css"
+  },
+  {
+    "revision": "b959fb7ffd3f71006fd1",
+    "url": "/gitsearch/static/js/2.79dcdd63.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gitsearch/static/js/2.79dcdd63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbecd2d262d70bd950d",
+    "url": "/gitsearch/static/js/main.af81b1f5.chunk.js"
+  },
+  {
+    "revision": "355034e308a294babae0",
+    "url": "/gitsearch/static/js/runtime-main.ffb2991a.js"
+  },
+  {
+    "revision": "73bfa12b55452a65a0253c511856d6c2",
+    "url": "/gitsearch/static/media/Gilroy-Light.73bfa12b.ttf"
+  },
+  {
+    "revision": "c2a5327dd2426ad4cbdd4868010d88dd",
+    "url": "/gitsearch/static/media/Gilroy-Light.c2a5327d.eot"
+  },
+  {
+    "revision": "f9b62dd8f7c5cc5820eee7743544d321",
+    "url": "/gitsearch/static/media/Gilroy-Light.f9b62dd8.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/gitsearch/static/media/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/gitsearch/static/media/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/gitsearch/static/media/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/gitsearch/static/media/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/gitsearch/static/media/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/gitsearch/static/media/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "4ab1c0257488082f94f43a51b416ce17",
+    "url": "/gitsearch/static/media/logo.4ab1c025.svg"
+  }
+]);
